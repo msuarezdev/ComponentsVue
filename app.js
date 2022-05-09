631055e9ac43bb521b1a1900e62b02c38161b664
Mati Suarez <msuarez.developer@gmail.com>
2022-05-08 21:35:41 -0300

    upgrade

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ Vue.component("tabla",{
         }
     },
     template:`
-    <div>
-    <table>
-        <thead>
+    <div class="table-responsive">
+    <table class="table table-bordered table-hover">
+        <thead class="thead__color">
             <tr>
                 <th v-for="(item,i) in titulos" :key="i" scope="col">{{ item }}</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="(datosCripto, i) in registros" :key="i">
-                <th v-for="(dato, d) in datosCripto" :key="d">{{ dato }}</th>
+                <th v-for="(dato, d) in datosCripto" :key="d" :style="{'color':'#f2f2f2'}">{{ dato }}</th>
             </tr>
         </tbody>
     </table>
@@ -33,12 +33,23 @@ let app = new Vue({
         tablaCripto: {
             titulos: [" ","Bitcoin (BTC)", "Etherum (ETH)", "Cardano (ADA)", "Solana (SOL)", "Tether (USDT)"],
             registros: [
-                ["Año de creación", "2009", "2015", "", "", ""],
+                ["Año de creación", "2009", "2015", "2015", "2017", "2014"],
                 ["Descentralizado", "sí", "sí", "sí", "sí", "no"],
-                ["Emisión Total", "21.000.00", "ilimitada", "", "", ""],
-                ["Circulación", "18.400.700", "111.297.108", "", "", ""],
-                ["Protocolo", "poW", "PoW", "", "", ""],
+                ["Emisión Total", "21.000.00", "ilimitada", "45.000.000.000", "500.000.000", "ilimitada"],
+                ["Circulación", "19.021.550", "111.297.108", "33.752.565.071", "335.187.218", "83.217.377.095"],
+                ["Protocolo", "poW", "PoW", "poS", "poS", "Omni"],
+                ["Minería", "sí", "sí", "no", "no", "no",],
+                ["Puesto en cap de mercado", "#1", "#2", "#8", "#7", "#3"],
+                ["Precio máximo historico", "$65.466,84", "$4.651,46", "$2,96", "$158,96", "1,027"],
+                ["Tiempo de bloque(promedio)", "10min", "15seg", "10min", "0,4seg", "2min"],
             ]
+        },
+        titulo: "Diferencias entre criptomonedas",
+    
+    },
+    method:{ 
+    clasesCss: {
+        "colorText":true,
         },
     },
 });
